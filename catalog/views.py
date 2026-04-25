faaa1162978255f16a0366b90df6b4234a022d5c
Дмитрий Глоба <Dima.globa2015@yandex.ru>
2026-04-25 17:03:15 +0800

    Добавлены статики

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def contacts(request):
+    """Контролер страницы contacts.html"""
+    return render(request, 'catalog/contacts.html')
+
+def home(request):
+    """Контролер страницы home.html"""
+    return render(request, 'catalog/home.html')
