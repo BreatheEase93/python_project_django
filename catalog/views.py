59d9f39930ea2b7c3befa9cecacac9e56b7618b2
Дмитрий Глоба <Dima.globa2015@yandex.ru>
2026-04-25 17:41:42 +0800

    Задача *

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def contacts(request):
-    """Контролер страницы contacts.html"""
+    """Контролер страницы contacts.html с обратной связью"""
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        return HttpResponse (f"Здравствуйте, {name}! Мы с вами свяжемся!")
     return render(request, 'catalog/contacts.html')
 
 def home(request):
