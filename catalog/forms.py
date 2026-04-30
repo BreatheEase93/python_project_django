287ac194c21ff5301aee8bf310a2633812e8b3f3
Дмитрий Глоба <dima.globa2015@yandex.ru>
2026-04-30 16:36:21 +0800

    задание 1

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,6 +3,11 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    FORBIDDEN_WORDS = [
+        'казино', 'криптовалюта', 'крипта', 'биржа',
+        'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+    ]
+
     class Meta:
         model = Product
         fields = ('name', 'description', 'image', 'category', 'price',)
@@ -14,3 +19,17 @@ class ProductForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'form-control'}),
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        for word in self.FORBIDDEN_WORDS:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError(f'Название содержит запрещенное слово: "{word}"')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        for word in self.FORBIDDEN_WORDS:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError(f'Описание содержит запрещенное слово: "{word}"')
+        return cleaned_data
