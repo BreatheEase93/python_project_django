85a61d163c3b4abdcc75eae7a3d5b6cefcc5f429
Дмитрий Глоба <dima.globa2015@yandex.ru>
2026-05-08 20:32:19 +0800

    работа готова

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Личные данные', {'fields': ('username', 'first_name', 'last_name', 'phone_number', 'avatar', 'country')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
